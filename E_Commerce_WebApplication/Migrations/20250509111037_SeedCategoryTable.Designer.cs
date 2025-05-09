0b4136da9212338b72f0f1d68273710c155ab23b
Kadir Kobak <kadir.kobak@celsan.com>
2025-05-09 14:12:04 +0300

    update

--- E_Commerce_WebApplication/Migrations/20250509111037_SeedCategoryTable.Designer.cs
+++ E_Commerce_WebApplication/Migrations/20250509111037_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using E_Commerce_WebApplication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_Commerce_WebApplication.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250509111037_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
