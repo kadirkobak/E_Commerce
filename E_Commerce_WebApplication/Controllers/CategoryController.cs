65bc2790a22ee8a6d3df3657c48f0ecbc5ba112d
Kadir Kobak <kadir.kobak@celsan.com>
2025-05-09 14:25:56 +0300

    update

--- E_Commerce_WebApplication/Controllers/CategoryController.cs
+++ E_Commerce_WebApplication/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce_WebApplication.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_WebApplication.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
+
+            var objCategoryList = _dbContext.Categories.ToList();  
             return View();
         }
     }
