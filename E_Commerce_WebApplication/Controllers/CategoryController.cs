c0b15b48f533aee01c98b0274fb1efae91d320e3
Kadir Kobak <kadir.kobak@celsan.com>
2025-05-16 11:43:11 +0300

    update

--- E_Commerce_WebApplication/Controllers/CategoryController.cs
+++ E_Commerce_WebApplication/Controllers/CategoryController.cs
@@ -16,18 +16,18 @@ namespace E_Commerce_WebApplication.Controllers
         public IActionResult Index()
         {
 
-            var objCategoryList = _dbContext.Categories.ToList();  
+            var objCategoryList = _dbContext.Categories.ToList();
             return View(objCategoryList);
         }
-       
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
-        
+
         [HttpPost]
-        public IActionResult Create( Category obj)
+        public IActionResult Create(Category obj)
         {
             if (ModelState.IsValid)
             {
@@ -37,7 +37,37 @@ namespace E_Commerce_WebApplication.Controllers
             }
 
             return View();
-           
+
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _dbContext.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        /*[HttpPost]
+        public IActionResult Edit(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(id);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }*/
     }
 }
