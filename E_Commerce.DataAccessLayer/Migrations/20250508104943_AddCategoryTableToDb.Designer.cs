1a552ecddf7e9375ecbe39e36157a9ba8b5070e2
Kadir Kobak <kadir.kobak@celsan.com>
2025-05-27 13:49:04 +0300

    update

--- E_Commerce.DataAccessLayer/Migrations/20250508104943_AddCategoryTableToDb.Designer.cs
+++ E_Commerce.DataAccessLayer/Migrations/20250508104943_AddCategoryTableToDb.Designer.cs
@@ -19,7 +19,7 @@ namespace E_Commerce_DataAccessLayer.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.4")
+                .HasAnnotation("ProductVersion", "9.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
