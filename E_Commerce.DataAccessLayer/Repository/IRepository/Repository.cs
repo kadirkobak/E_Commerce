1f505e5ef43683bd06948ba2575d500f171ec6af
Kadir Kobak <kadir.kobak@celsan.com>
2025-05-28 10:01:06 +0300

    update

---
+++ E_Commerce.DataAccessLayer/Repository/IRepository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using E_Commerce_DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DataAccessLayer.Repository.IRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            this.dbSet = _dbContext.Set<T>();
+        }
+
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+ 
+  
+}
