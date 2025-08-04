887917cb18e58edf32ac6458e4a7918f8a92e428
Kadir Kobak <kadir.kobak@celsan.com>
2025-08-04 17:51:02 +0300

    update

--- E_Commerce.DataAccessLayer/Repository/ProductRepository.cs
+++ E_Commerce.DataAccessLayer/Repository/ProductRepository.cs
@@ -28,16 +28,16 @@ namespace E_Commerce.DataAccessLayer.Repository
 
             if (objFromDb != null)
             {
-                objFromDb.Title = obj.Title;
-                objFromDb.Description = obj.Description;
-                objFromDb.ISBN = obj.ISBN;
-                objFromDb.Author = obj.Author;
-                objFromDb.ListPrice = obj.ListPrice;
-                objFromDb.Price = obj.Price;
+                objFromDb.Title = obj.Title; 
+                objFromDb.Description = obj.Description; 
+                objFromDb.ISBN = obj.ISBN; 
+                objFromDb.Author = obj.Author; 
+                objFromDb.ListPrice = obj.ListPrice; 
+                objFromDb.Price = obj.Price; 
                 objFromDb.Price50 = obj.Price50;
-                objFromDb.Price100 = obj.Price100;
+                objFromDb.Price100 = obj.Price100; 
                 objFromDb.CategoryId = obj.CategoryId;
-
+                
                 if (obj.ImageUrl != null)
                 {
                     objFromDb.ImageUrl = obj.ImageUrl;
