e1bcbd177ddb956078d20789a00edab1721fd46a
Kadir Kobak <kadir.kobak@celsan.com>
2025-08-04 16:36:05 +0300

    update

--- E_Commerce.DataAccessLayer/Repository/ProductRepository.cs
+++ E_Commerce.DataAccessLayer/Repository/ProductRepository.cs
@@ -12,7 +12,7 @@ namespace E_Commerce.DataAccessLayer.Repository
 {
     public class ProductRepository : Repository<Product>, IProductRepository
     {
-      
+
         private readonly ApplicationDbContext _dbContext;
 
         public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
@@ -24,7 +24,27 @@ namespace E_Commerce.DataAccessLayer.Repository
 
         public void Update(Product obj)
         {
-            _dbContext.Products.Update(obj);
+            var objFromDb = _dbContext.Products.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Author = obj.Author;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.CategoryId = obj.CategoryId;
+
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+
+            }
+
         }
     }
 }
