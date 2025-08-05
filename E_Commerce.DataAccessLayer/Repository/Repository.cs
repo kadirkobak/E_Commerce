dca2d257aeb24528ef72a614d4a70138814bb8f0
Kadir Kobak <kadir.kobak@celsan.com>
2025-08-05 10:09:27 +0300

    update

--- E_Commerce.DataAccessLayer/Repository/Repository.cs
+++ E_Commerce.DataAccessLayer/Repository/Repository.cs
@@ -20,25 +20,44 @@ namespace E_Commerce.DataAccessLayer.Repository
         {
             _dbContext = dbContext;
             dbSet = _dbContext.Set<T>();
+            _dbContext.Products.Include(u => u.Category).Include(u => u.CategoryId); 
         }
 
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if(!string.IsNullOrEmpty(includeProperties))
+            {
+
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+
+            }
             return query.ToList();
         }
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+
+            }
             return query.FirstOrDefault();
         }
         public void Add(T entity)
         {
             dbSet.Add(entity);
         }
-     
+
         public void Remove(T entity)
         {
             dbSet.Remove(entity);
@@ -48,6 +67,6 @@ namespace E_Commerce.DataAccessLayer.Repository
             dbSet.RemoveRange(entity);
         }
     }
- 
-  
+
+
 }
--- E_Commerce_WebApplication/Areas/Admin/Controllers/ProductController.cs
+++ E_Commerce_WebApplication/Areas/Admin/Controllers/ProductController.cs
@@ -24,7 +24,7 @@ namespace E_Commerce_WebApplication.Areas.Admin.Controllers
         public IActionResult Index()
         {
 
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
 
             return View(objProductList);
         }
