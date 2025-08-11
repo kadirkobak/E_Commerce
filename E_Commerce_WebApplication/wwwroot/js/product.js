eaa11849c6380134b6dcb9a4758f8516fa854dd9
Kadir Kobak <kadir.kobak@celsan.com>
2025-08-11 16:29:32 +0300

    update

--- E_Commerce_WebApplication/wwwroot/js/product.js
+++ E_Commerce_WebApplication/wwwroot/js/product.js
@@ -1,24 +1,34 @@
-﻿
-
-$(document).ready(function () {
-    loaddataTable();
+﻿$(document).ready(function () {
+    loadDataTable();
 });
 
-function loaddataTable() {
+function loadDataTable() {
     $('#tblData').DataTable({
         "ajax": {
             "url": "/admin/product/getall",
-            "dataSrc": ""     },
-   
+        },
         "columns": [
-            { "data": "name", "width": "%15" },
-            { "data": "position", "width": "%15" },
-            { "data": "salary", "width": "%15" },
-            { "data": "office", "width": "%15" }
+            { "data": "title", "width": "25%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "listPrice", "width": "10%" },
+            { "data": "author", "width": "20%" },
+            { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+                            <a href="/admin/product/delete?id=${data}" class="btn btn-danger mx-2">
+                                <i class="bi bi-trash-fill"></i> Delete
+                            </a>
+                        </div>
+                    `
+                },
+                "width": "15%"
+            }
         ]
     });
-
-
 }
-
-
