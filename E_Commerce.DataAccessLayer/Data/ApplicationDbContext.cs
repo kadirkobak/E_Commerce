f031381de853d6732d7147bb9c20365e17db0351
Kadir Kobak <kadir.kobak@celsan.com>
2025-08-19 11:55:21 +0300

    update

--- E_Commerce.DataAccessLayer/Data/ApplicationDbContext.cs
+++ E_Commerce.DataAccessLayer/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using E_Commerce.Models;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Commerce_DataAccessLayer.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
